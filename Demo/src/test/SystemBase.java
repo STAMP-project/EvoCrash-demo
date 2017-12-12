d391ac093b2a0e8a1cbfd4b36d9d3146d5cf2858
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2017-12-12 11:46:29 +0100

    add cases: XRENDERING-422, XWIKI-13031, XWIKI-13196, XWIKI-13916

---
+++ Demo/src/test/SystemBase.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2017 Mozhan Soltani, Annibale Panichella, and Arie van Deursen
+ *
+ * This file is part of EvoCrash.
+ *
+ * EvoCrash is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3.0 of the License, or
+ * (at your option) any later version.
+ *
+ * EvoCrash is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with EvoSuite. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package test;
+
+import org.evosuite.ga.metaheuristics.GeneticAlgorithm;
+import org.evosuite.result.TestGenerationResult;
+
+import java.util.List;
+
+public class SystemBase {
+
+	@SuppressWarnings("unchecked")
+	protected GeneticAlgorithm<?> getGAFromResult(Object result) {
+		assert(result instanceof List);
+		List<List<TestGenerationResult>> results = (List<List<TestGenerationResult>>)result;
+		if(results.size()>0) {
+			return results.get(0).get(0).getGeneticAlgorithm();
+		}else {
+			return null;
+		}
+		
+    }
+}
